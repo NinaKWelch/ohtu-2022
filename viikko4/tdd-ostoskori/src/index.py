d044a715266fdfc73617604efc881cc82096d142
NinaWelch <nwelch1@live.co.uk>
2022-11-25 14:06:48 +0000

    test: add one item

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -7,6 +7,8 @@ def main():
     my_product = Tuote("maito", 3)
 
     my_basket.lisaa_tuote(my_product)
+    print(my_basket.tavaroita_korissa())
+   
 
 if __name__ == "__main__":
     main()
--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,22 +3,28 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self._ostokset = []
 
     def tavaroita_korissa(self):
-        return 0
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
+        tavaramaara = map(lambda o: o.lukumaara(), self._ostokset)
 
+        if len(self._ostokset) == 0:
+            return 0
+        
+        return sum(tavaramaara)
+ 
     def hinta(self):
         return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        pass
+        ostos = Ostos(lisattava)
+        self._ostokset.append(ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -27,8 +33,9 @@ class Ostoskori:
     def tyhjenna(self):
         pass
         # tyhjentää ostoskorin
-
+    
     def ostokset(self):
         pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+
