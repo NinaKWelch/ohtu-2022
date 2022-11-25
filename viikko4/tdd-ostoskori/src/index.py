e7ba0ae595822029d042380c2491bfcc28eb891f
NinaWelch <nwelch1@live.co.uk>
2022-11-25 14:39:03 +0000

    test: add two items to cart

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -4,11 +4,12 @@ from tuote import Tuote
 
 def main():
     my_basket = Ostoskori()
-    my_product = Tuote("maito", 3)
-
-    my_basket.lisaa_tuote(my_product)
+    my_product_1 = Tuote("Maito", 3)
+    my_product_2 = Tuote("Leipä", 5)
+    my_basket.lisaa_tuote(my_product_1)
+    my_basket.lisaa_tuote(my_product_2)
     print(my_basket.tavaroita_korissa())
-    print(my_basket.hinta())
+    #print(my_basket.hinta())
 
 if __name__ == "__main__":
     main()
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -15,3 +15,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_ykahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa_(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
