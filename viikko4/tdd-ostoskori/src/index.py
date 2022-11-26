4b9ab45b01cb404ec267dd9cd2575935b07ae235
NinaWelch <nwelch1@live.co.uk>
2022-11-26 18:17:33 +0000

    test: number of items in cart

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -7,9 +7,9 @@ def main():
     my_product_1 = Tuote("Maito", 3)
     my_product_2 = Tuote("Leipä", 5)
     my_basket.lisaa_tuote(my_product_1)
-    my_basket.lisaa_tuote(my_product_1)
     print(my_basket.tavaroita_korissa())
     print(my_basket.hinta())
+    print(my_basket.ostokset())
 
 if __name__ == "__main__":
     main()
